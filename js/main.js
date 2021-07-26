626121332fa012a13f7ddc5f51b037ad626c3d3b
Ellays <nata3287@gmail.com>
2021-07-26 12:54:00 +0300

    26.07.2021

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
+    $(".header__nav a").on("click", function (e) {
+                e.preventDefault();
+                var id  = $(this).attr('href'),
+                    top = $(id).offset().top;
+                $('body,html').animate({scrollTop: top}, 1500);
+            });
+        
+
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
@@ -14,9 +22,10 @@ $(function() {
         ]
     });
 
-    $('.nav__btn').on('click', function (){
+    $('.nav__btn, .nav__list a').on('click', function (){
         $('.header__top-inner').toggleClass('header__top-inner--active');
     });
+    
 
     var mixer = mixitup('.portfolio__content');
 
